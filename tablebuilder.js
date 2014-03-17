ff44f0235e80a63a4dfc711f98f234f908cd7c18
bocampbell <bo.j.campbell@gmail.com>
2014-03-17 12:34:02 -0700

    Started HTML and js files for testing

--- tablebuilder.js
+++ tablebuilder.js
@@ -1,1 +1,25 @@
-seed
+// create input that will later be pulled from textarea or input field 
+
+var aColors = new Array();
+aColors[0]="000000"
+aColors[1]="CCCCCC"
+aColors[2]="FFFFFF"
+
+function buildtable() {
+
+// for loop through the total number of colors 
+var x = aColors.length
+for (var i=0;i<x;i++)
+	document.write(aColors[i] + "<br>";)
+
+	// for loop through each color
+	
+		// apply formula to each combination
+		
+	// next
+	
+	// build row
+	
+next
+
+}
